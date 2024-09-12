2f7a416301b44472e751a5871ac4fa1b111a2184
Ansh Gupta <techbire69@gmail.com>
2024-09-12 23:44:41 +0530

    Time: 550 ms (7.20%), Space: 17.4 MB (25.13%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,58 +9,17 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (headA == nullptr) return headA;
-        if (headB == nullptr) return headB;
-
-        unordered_set<ListNode*> nodeAddress;
-
-        while (headA != nullptr) {
-            nodeAddress.insert(headA);
-            headA = headA->next;
+       ListNode *t1=headA;
+        while(t1!=nullptr){
+            ListNode *t2=headB;
+            while(t2!=nullptr){
+                if(t1==t2){
+                    return t1;
+                }
+                t2=t2->next;
+            }
+            t1=t1->next;
         }
-
-        while (headB != nullptr) {
-            if (nodeAddress.find(headB) != nodeAddress.end())
-                return headB;
-
-            headB = headB->next;
-        }
-
-        return nullptr;
-    }
-
-    ListNode* getIntersectionNodeLength(ListNode* headA, ListNode* headB) {
-        int lenA = getListLength(headA);
-        int lenB = getListLength(headB);
-
-        while (lenA > lenB) {
-            lenA--;
-            headA = headA->next;
-        }
-
-        while (lenB > lenA) {
-            lenB--;
-            headB = headB->next;
-        }
-
-        // Now both heads are at the same distance from intersection
-        // Start moving them both until they meet
-        while (headA != headB) {
-            headA = headA->next;
-            headB = headB->next;
-        }
-
-        return headA;
-    }
-
-    int getListLength(ListNode* head) {
-        int len = 0;
-
-        while (head != nullptr) {
-            len++;
-            head = head->next;
-        }
-
-        return len;
+        return NULL;
     }
 };
