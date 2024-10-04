a8bb217302be227a3577f2068f7a9942616303f2
Ansh Gupta <techbire69@gmail.com>
2024-10-04 21:39:19 +0530

    Time: 54 ms (92.60%), Space: 55.6 MB (99.80%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -16,14 +16,17 @@ public:
         }
 
         long long ans=0;
+        // Forming pairs
         for(int i=0;i<skill.size();i++){
             int half = reqSum - skill[i];
+             // Check if current skill and required skill are both available
             if(count[half] > 0){
                 ans += skill[i] * half;
                 count[half]--;
             }
             else return -1;
         }
+        // Return total sum of products divided by 2 (since each pair is counted twice)
         return ans/2;
     }
 };
