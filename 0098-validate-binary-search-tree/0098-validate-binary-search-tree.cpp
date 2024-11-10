76941473b883f385df1d931511e24e4abdfb158e
Ansh Gupta <techbire69@gmail.com>
2024-11-10 23:37:07 +0530

    Time: 0 ms (100.00%), Space: 20.5 MB (52.41%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,11 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root, TreeNode* minNode = nullptr, TreeNode* maxNode = nullptr) {
+//         if (root == nullptr) return true;
+//         if ((minNode && root->val <= minNode->val) || (maxNode && root->val >= maxNode->val)) return false;
+//         return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+//     }
+// };
+
+//simple solution
 class Solution {
 public:
-    bool isValidBST(TreeNode* root, TreeNode* minNode = nullptr, TreeNode* maxNode = nullptr) {
-        if (root == nullptr) return true;
-        if ((minNode && root->val <= minNode->val) || (maxNode && root->val >= maxNode->val)) return false;
-        return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, LONG_MIN, LONG_MAX);
+    }
+
+private:
+    bool isValidBST(TreeNode* root, long minVal, long maxVal) {
+        if (!root) return true;
+        if (root->val <= minVal || root->val >= maxVal) return false;
+        return isValidBST(root->left, minVal, root->val) && isValidBST(root->right, root->val, maxVal);
     }
 };
