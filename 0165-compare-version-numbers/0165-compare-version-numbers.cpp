19135c9a2cfd09e4ce9e13b441713e1e4725da0b
Ansh Gupta <techbire69@gmail.com>
2025-09-23 23:49:01 +0530

    Time: 0 ms (100%), Space: 8.2 MB (87.43%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int i = 0, j = 0;
+        int n1 = v1.size(), n2 = v2.size();
+
+        while (i < n1 || j < n2) {
+            long num1 = 0, num2 = 0;
+
+            while (i < n1 && v1[i] != '.') {
+                num1 = num1 * 10 + (v1[i] - '0');
+                i++;
+            }
+
+            while (j < n2 && v2[j] != '.') {
+                num2 = num2 * 10 + (v2[j] - '0');
+                j++;
+            }
+
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+
+            i++;
+            j++;
+        }
+        
+        return 0;
+    }
+};
