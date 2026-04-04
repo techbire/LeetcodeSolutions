c59a6a5bfef4af86a4bd56187da359b924294cb0
Ansh Gupta <techbire69@gmail.com>
2026-04-04 13:20:36 +0530

    Time: 0 ms (100%), Space: 9.9 MB (8.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool isValid(string s) {
-    //logic-last opened then first to be closed
-    stack<char>st;
-    for(char ch:s){
-        if(ch=='('||ch=='['||ch=='{'){
-            st.push(ch);
+        stack<char> st;
+
+        for(char ch : s) {
+            if(ch == '(') st.push(')');
+            else if(ch == '{') st.push('}');
+            else if(ch == '[') st.push(']');
+            else if(st.empty() || st.top() != ch) return false;
+            else st.pop();
         }
-        else{
-            if(st.empty())return false;
-            char top=st.top();
-            st.pop();
-        if (ch == ')' && top != '(') return false;
-                    if (ch == ']' && top != '[') return false;
-                    if (ch == '}' && top != '{') return false;
-                }
-            }
-            return st.empty();
+
+        return st.empty();
     }
 };
