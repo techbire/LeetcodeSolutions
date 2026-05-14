01463c361c64db8114f0c2a710de8726ccb160e8
Ansh Gupta <techbire69@gmail.com>
2026-05-14 15:18:17 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,12 +2,29 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-        return true;
-        }
-        else return false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t){
+        // return true;
+        // }
+        // else return false;
         
+
+
+        //voting method approach 
+        if (s.length() != t.length()) return false;
+
+        int arr[26] = {0};
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) return false;
+        }
+
+        return true;
     }
 };
