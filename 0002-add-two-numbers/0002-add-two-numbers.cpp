f1661c9c348061526472f8abf4134b5c7657aa42
Ansh Gupta <techbire69@gmail.com>
2026-06-10 20:57:54 +0530

    Time: 3 ms (34.82%), Space: 77 MB (73.88%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,31 +10,33 @@
  */
 class Solution {
 public:
- ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummy = new ListNode();
-        ListNode *curr = dummy;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
 
-        while(l1 != NULL || l2 != NULL) {
-            int val1 = (l1 != NULL) ? l1->val : 0;
-            int val2 = (l2 != NULL) ? l2->val : 0;
+        ListNode* root = new ListNode(0);
+        ListNode* curr = root;
 
-            int sum = val1 + val2 + carry;
-            carry = sum/10;
+        while (l1 || l2 || carry) {
+            int v1 = 0, v2 = 0;
 
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
-//reversing the linklist
-            if (l1 != NULL) {
+            if (l1) {
+                v1 = l1->val;
                 l1 = l1->next;
             }
-            if (l2 != NULL) {
+
+            if (l2) {
+                v2 = l2->val;
                 l2 = l2->next;
             }
-        }
-        if(carry == 1)
-            curr->next = new ListNode(1);
 
-        return dummy->next;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            int val = sum % 10;
+
+            curr->next = new ListNode(val);
+            curr = curr->next;
+        }
+
+        return root->next;
     }
 };
